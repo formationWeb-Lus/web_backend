e079d7ff6bd6cd564dececfdce4acb38e9fa30d9
formationWeb-Lus <jiresselusa127@gmail.com>
2025-05-26 07:10:38 -0700

    ading

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const session = require("express-session");
+const pool = require('./database/'); // ou selon ton fichier de connexion DB
+
 require('dotenv').config()
 
 const express = require("express")
@@ -15,6 +18,43 @@ const app = express()
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout")
+
+
+
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 
 /* ***********************
  * Middleware to inject nav
