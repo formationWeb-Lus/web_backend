195fb81a4f1e10edd646de46ab9e88085d51d62f
formationWeb-Lus <jiresselusa127@gmail.com>
2025-05-26 07:53:37 -0700

    adding

--- database/connection.js
+++ database/connection.js
@@ -1,12 +1,10 @@
-const { Pool } = require('pg')
+const { Pool } = require("pg")
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    require: true,
-    rejectUnauthorized: false
+    rejectUnauthorized: false, // nécessaire sur Render
   }
 })
 
 module.exports = pool
-
